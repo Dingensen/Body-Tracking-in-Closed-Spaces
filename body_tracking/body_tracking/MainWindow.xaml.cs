1765887a42561c80f55992ad20d2c32d34b167b3
Dingensen <f_nieb02@uni-muenster.de>
2021-06-15 14:01:57 +0200

    exercise2

--- body_tracking/body_tracking/MainWindow.xaml.cs
+++ body_tracking/body_tracking/MainWindow.xaml.cs
@@ -23,14 +23,113 @@ namespace body_tracking
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Kinect Sensor
         private KinectSensor kinectSensor = null;
+
+        /** INFRARED FRAME variable initialisations **/
+        //reader for infrared frames
+        private InfraredFrameReader infraredFrameReader = null;
+        private FrameDescription infraredFrameDescription = null;
+        private WriteableBitmap bitmap = null;
+
+        //TODO: later on, bind these variables to buttons to change them at runtime if possible
+
+        /// Setup the limits (post processing)of the infrared data that we will render.
+        /// Increasing or decreasing this value sets a brightness "wall" either closer or further away.
+        private const float InfraredOutputValueMinimum = 0.01f;
+        private const float InfraredOutputValueMaximum = 1.0f;
+        private const float InfraredSourceValueMaximum = ushort.MaxValue;
+        /// Scale of Infrared source data
+        private const float InfraredSourceScale = 0.75f;
+
         public MainWindow()
         {
             // one sensor is currently supported
             this.kinectSensor = KinectSensor.GetDefault();
+
+            //open depth frame reader
+            this.infraredFrameReader = kinectSensor.InfraredFrameSource.OpenReader();
+            // wire handler for frame arrival - This is a defined method
+            this.infraredFrameReader.FrameArrived += Reader_InfraredFrameArrived;
+            // get FrameDescription from InfraredFrameSource
+            this.infraredFrameDescription = kinectSensor.InfraredFrameSource.FrameDescription;
+
+            // create the bitmap to display
+            this.bitmap = new WriteableBitmap(infraredFrameDescription.Width, infraredFrameDescription.Height, 96.0, 96.0, PixelFormats.Gray32Float, null);
+
+            // use the window object as the view model in this simple example
+            this.DataContext = this;
+
             // open the sensor
             this.kinectSensor.Open();
             InitializeComponent();
+        }
+
+        public ImageSource ImageSource
+        {
+            get
+            {
+                return this.bitmap;
+            }
+        }
+
+        /// <summary>
+        /// Handles the infrared frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_InfraredFrameArrived(object sender, InfraredFrameArrivedEventArgs e)
+        {
+
+            // InfraredFrame is IDisposable
+            using (InfraredFrame infraredFrame = e.FrameReference.AcquireFrame())
+            {
+                if (infraredFrame != null)
+                {
+                    /// We are using WPF (Windows Presentation Foundation)
+                    using (Microsoft.Kinect.KinectBuffer infraredBuffer = infraredFrame.LockImageBuffer())
+                    {
+                        // verify data and write the new infrared frame data to the display bitmap
+                        if (((this.infraredFrameDescription.Width * this.infraredFrameDescription.Height) == (infraredBuffer.Size / this.infraredFrameDescription.BytesPerPixel)) &&
+                            (this.infraredFrameDescription.Width == this.bitmap.PixelWidth) && (this.infraredFrameDescription.Height == this.bitmap.PixelHeight))
+                        {
+                            this.ProcessInfraredFrameData(infraredBuffer.UnderlyingBuffer, infraredBuffer.Size);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        /// Directly accesses the underlying image buffer of the InfraredFrame to create a displayable bitmap.
+        /// This function requires the /unsafe compiler option as we make use of direct access to the native memory pointed to by the infraredFrameData pointer.
+        /// Activate "unsafe" in the solution properties > on the left >Build > Check Allow unsafe code
+        /// <param name="infraredFrameData">Pointer to the InfraredFrame image data</param>
+        /// <param name="infraredFrameDataSize">Size of the InfraredFrame image data</param>
+        private unsafe void ProcessInfraredFrameData(IntPtr infraredFrameData, uint infraredFrameDataSize)
+        {
+            // infrared frame data is a 16 bit value
+            ushort* frameData = (ushort*)infraredFrameData;
+
+            // lock the target bitmap
+            this.bitmap.Lock();
+
+            // get the pointer to the bitmap's back buffer
+            float* backBuffer = (float*)this.bitmap.BackBuffer;
+
+            // process the infrared data
+            for (int i = 0; i < (int)(infraredFrameDataSize / this.infraredFrameDescription.BytesPerPixel); ++i)
+            {
+                // since we are displaying the image as a normalized grey scale image, we need to convert from
+                // the ushort data (as provided by the InfraredFrame) to a value from [InfraredOutputValueMinimum, InfraredOutputValueMaximum]
+                backBuffer[i] = Math.Min(InfraredOutputValueMaximum, (((float)frameData[i] / InfraredSourceValueMaximum * InfraredSourceScale) * (1.0f - InfraredOutputValueMinimum)) + InfraredOutputValueMinimum);
+            }
+
+            // mark the entire bitmap as needing to be drawn
+            this.bitmap.AddDirtyRect(new Int32Rect(0, 0, this.bitmap.PixelWidth, this.bitmap.PixelHeight));
+
+            // unlock the bitmap
+            this.bitmap.Unlock();
         }
     }
 }
